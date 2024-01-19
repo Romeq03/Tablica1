d8fedf27d57f7741fdfef967a0e6d5b01fc9cce0
Romeq03 <romekbartek18@gmail.com>
2024-01-19 18:48:10 +0100

    first commit

---
+++ Tablica1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Tworzenie tablicy jednowymiarowej o nazwie tablica1
+        int[] tablica1 = new int[10];
+
+        // Wypełnianie tablicy liczbami od 0 do 9
+        for (int i = 0; i < 10; i++)
+        {
+            tablica1[i] = i;
+        }
+
+        // Wyświetlanie zawartości tablicy
+        Console.WriteLine("Zawartość tablicy1:");
+        foreach (int liczba in tablica1)
+        {
+            Console.Write(liczba + " ");
+        }
+
+        // Jeśli chcesz zobaczyć reprezentację graficzną tablicy, możesz użyć poniższej linii:
+        // Console.WriteLine(string.Join(" ", tablica1));
+    }
+}
